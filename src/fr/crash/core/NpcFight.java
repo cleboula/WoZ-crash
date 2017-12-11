dcc3f54bd555984bbf24efd74601532ec13bf7d4
Shahine <shahine.aqechmar.gbi@gmail.com>
2017-12-11 14:33:41 +0100

    Java doc NPCs

--- src/fr/crash/core/NpcFight.java
+++ src/fr/crash/core/NpcFight.java
@@ -9,37 +9,55 @@ package fr.crash.core;
 public abstract class NpcFight extends Npc {
 	
 	private int hp;
-	//Npc Fighters Health points
 	private int atk;
-	//Npc Fighters Attack Score
-	
-	
-	//NpcFight constructor
+
+
+	/**
+	 * Constructor of NpcDialog
+	 * @param hp1 the Npc health points
+	 * @param atk1 the Npc attack score
+	 * @param name the Npc name
+	 * @param description the Npc description
+	 */
 	public NpcFight(int hp1,int atk1,String name,String description){
 		super(name, description);
 		hp=hp1;
 		atk=atk1;
 	}
-	
-	//The canvas for the attackPattern method that implement the Fighter Behavior
+
+	/**
+	 * Abstract method for the pattern of attacks of the monsters
+	 */
 	public abstract int attackPattern();
 
-	
-	//Hp getter
+
+	/**
+	 * This method is used to get the hp of the Npc.
+	 * @return     the hp of the Npc
+	 */
 	public int getHp() {
 		return hp;
 	}
-	//Atk getter
+	/**
+	 * This method is used to get the Atk of the Npc.
+	 * @return     the Atk of the Npc
+	 */
 	public int getAtk() {
 		return atk;
 	}
-	
-	//Hp setter
+
+	/**
+	 * Setter of the current Hp
+	 * @param hp the new hp points
+	 */
 	public void setHp(int hp) {
 		this.hp = hp;
 	}
 
-	//Atk setter
+	/**
+	 * Setter of the current atk
+	 * @param atk the new atk Spoints
+	 */
 	public void setAtk(int atk) {
 		this.atk = atk;
 	}
