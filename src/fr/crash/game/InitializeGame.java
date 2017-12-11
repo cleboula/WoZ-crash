a6e357fcea7fcee634864f5ae4005823c35622e1
gomar97 <margot.1997@hotmail.fr>
2017-12-11 17:33:14 +0100

    modification du medikit magic lake en magic Lake water

--- src/fr/crash/game/InitializeGame.java
+++ src/fr/crash/game/InitializeGame.java
@@ -95,7 +95,7 @@ public InitializeGame() {
 			
 			//creation of the medikits
 			medChurch = new Medikit(60, 40, "Small Medikit", "This is a medikit. Use it carefully!",null);
-			medLake = new Medikit(30,30,"Magic Lake", "This lake is refreshing. Oh, it can heal you! Come back as much as you need.",null);
+			medLake = new Medikit(30,30,"Magic Lake Water", "This lake is refreshing. Oh, it can heal you! Come back as much as you need.",null);
 	
 			// creation of the keys
 			//keys to unlock the paths
